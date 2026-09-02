b9538526e8858a681a0cc204b5bef2b80ba32149
Ahmad Tallouj <ahmadtallouj9@gmail.com>
2026-09-01 23:04:53 -0700

    fix(bootstrap): use non-obsolete FindObjectsByType signature

--- Assets/VYTHERA/Gameplay/Bootstrap/GameBootstrapper.cs
+++ Assets/VYTHERA/Gameplay/Bootstrap/GameBootstrapper.cs
@@ -126,7 +126,7 @@ namespace VYTHERA.Gameplay.Bootstrap
             }
 
             // Bind scene Sun to RenderSettings
-            var lights = FindObjectsByType<Light>(FindObjectsSortMode.None);
+            var lights = FindObjectsByType<Light>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
             for (int i = 0; i < lights.Length; i++)
             {
                 if (lights[i].type == LightType.Directional)
