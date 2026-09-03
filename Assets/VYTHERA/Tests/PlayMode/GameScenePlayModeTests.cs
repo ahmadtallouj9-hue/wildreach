ed9bff9fb2ee7aea47849d40c29a7d649e976937
Ahmad Tallouj <ahmadtallouj9@gmail.com>
2026-09-02 20:18:57 -0700

    fix(tests): use VYTHERA.Save namespace

--- Assets/VYTHERA/Tests/PlayMode/GameScenePlayModeTests.cs
+++ Assets/VYTHERA/Tests/PlayMode/GameScenePlayModeTests.cs
@@ -7,7 +7,7 @@ using VYTHERA.Gameplay.Bootstrap;
 using VYTHERA.Gameplay.Inventory;
 using VYTHERA.Gameplay.Survival;
 using VYTHERA.Player.Physics;
-using VYTHERA.Save.Serialization;
+using VYTHERA.Save;
 using VYTHERA.World.Streaming;
 
 namespace VYTHERA.Tests.PlayMode
